0e7deca153a4f0ab82cb45efc312687c72294d86
Dilip Nepali <er.dilip.n@gmail.com>
2022-01-04 23:49:41 -0500

    initial commit

---
+++ src/test/java/com/dockerk8s/jibmaven/JibMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dockerk8s.jibmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JibMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
